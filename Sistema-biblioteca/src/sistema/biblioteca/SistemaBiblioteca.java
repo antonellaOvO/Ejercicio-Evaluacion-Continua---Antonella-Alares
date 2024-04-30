0bc6ed11c19b9ae4d2f7bf764fabbe16ac710113
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2024-04-30 16:07:30 +0200

    added author name on files

--- Sistema-biblioteca/src/sistema/biblioteca/SistemaBiblioteca.java
+++ Sistema-biblioteca/src/sistema/biblioteca/SistemaBiblioteca.java
@@ -1,4 +1,4 @@
-/*Antonella Alares*/
+/*Autora: Antonella Alares*/
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
